bea5c0d8a6adb8ca9604a557ff90fba2c9ecf530
afsdari0 <dariorbneto2@hotmail.com>
2025-05-24 16:49:06 -0300

    Initial commit

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: MainContainer
+}
+</script>
+
+<style scoped>
+body{
+    height: 100vh;
+    width: 100vw;
+    background: white;
+}
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <MainContainer />
+</template>
+
+<script setup>
+import MainContainer from '@/components/MainContainer.vue';
+  //
+</script>
+<style scoped>
+body{
+  height: 100vh;
+  width: 100vw;
+}
+</style>
